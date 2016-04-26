462e009d521779fc8beedb92034eca98ac902e7d
王晓龙 <wangxiaolong@mobike.com>
2016-04-26 19:49:26 +0800

    添加脚本

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#脚本使用说明
+Usage(){
+	echo "Git 使用脚本
+	Usage ：
+	#add commit push 
+	./update.sh push commit 内容
+	./update.sh push  提交内容"
+}
+TODO=$1
+DESC=$2
+
+push(){
+	git  add .
+	git ci "$DESC"
+	git push os master
+	git push gb master
+}
+die(){
+	echo #print 
+	echo "$*"
+	Usage
+	echo
+	exit 1
+}
+case $TODO in
+	push )
+		push
+		;;
+	*)
+die	"参数错误"
+；；
+esac
